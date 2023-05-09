c9c261052c49715d4e3c87f1dc8f0356815c9e4b
Kushal Pagolu <kushal.pagolu@gmail.com>
2023-05-09 00:58:08 -0400

    Changed the alignment of icons to work well on mobile.

--- src/screens/FoodItem.js
+++ src/screens/FoodItem.js
@@ -24,123 +24,114 @@ export const renderFoodItem = ({ item, showItemDetails, shoppingCart, addToCart,
         colors={["rgba(0,0,0,0)", "rgba(0,0,0,0.6)"]}
         style={styles.foodItemOverlay}
       >
-              <View style={styles.foodItemDetails}>
-                <Text style={styles.foodItemName}>{item.itemName}</Text>
-                <View style={styles.bottomRow}>
-                  <View style={styles.categoryAndHeart}>
-                    <Text style={styles.foodItemCategory}>{item.categoryName}</Text>
-                    <Ionicons
-                      name="heart-outline"
-                      size={20}
-                      color="#FFF"
-                      style={styles.favoriteIcon}
-                    />
-                  </View>
-                  <View style={styles.iconsContainer}>
-                    <View style={styles.quantityContainer}>
-                    {itemQuantity > 0 && (
-                      <TouchableOpacity onPress={() => removeFromCart(item)}>
-                        <AntDesign name="minuscircleo" size={15} color="#FFF" style={styles.icons}/>
-                      </TouchableOpacity>
-                    )}
-                    {itemQuantity > 0 && (
-                      <Text style={styles.itemQuantity}>{itemQuantity}</Text>
-                    )}
-                    <TouchableOpacity onPress={() => addToCart(item)}>
-                      <AntDesign name="pluscircleo" size={15} color="#FFF" style={styles.icons}/>
-                    </TouchableOpacity>
-                  </View>
-                  </View>
-                </View>
+        <View style={styles.foodItemDetails}>
+          <View style={styles.topRow}>
+            <Text style={styles.foodItemName}>{item.itemName}</Text>
+            <Ionicons
+              name="heart-outline"
+              size={20}
+              color="#FFF"
+              style={styles.favoriteIcon}
+            />
+          </View>
+          <Text style={styles.foodItemCategory}>{item.categoryName}</Text>
+          <View style={styles.bottomRow}>
+            <View style={styles.iconsContainer}>
+              <View style={styles.quantityContainer}>
+                {itemQuantity > 0 && (
+                  <TouchableOpacity onPress={() => removeFromCart(item)}>
+                    <AntDesign name="minuscircleo" size={15} color="#FFF" style={styles.icons}/>
+                  </TouchableOpacity>
+                )}
+                {itemQuantity > 0 && (
+                  <Text style={styles.itemQuantity}>{itemQuantity}</Text>
+                )}
+                <TouchableOpacity onPress={() => addToCart(item)}>
+                  <AntDesign name="pluscircleo" size={15} color="#FFF" style={styles.icons}/>
+                </TouchableOpacity>
               </View>
-            </LinearGradient>
-          </TouchableOpacity>
-        );
-      };
-      const styles = StyleSheet.create({
-        foodItemContainer: {
-          flex: 1,
-          marginBottom: 16,
-          borderRadius: 10,
-          overflow: 'hidden',
-          marginTop: 5,
-          margin: 5,
-        },
-        bottomRow: {
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        },
-        categoryAndHeart: {
-          flexDirection: 'row',
-          alignItems: 'center',
-        },
-        foodItemImage: {
-          width: '100%',
-          height: 200,
-        },
-        foodItemOverlay: {
-          position: 'absolute',
-          bottom: 0,
-          left: 0,
-          right: 0,
-          padding: 16,
-        },
-        
-        foodItemName: {
-          color: 'white',
-          fontWeight: 'bold',
-          fontSize: 18,
-        },
-        foodItemCategory: {
-          color: 'white',
-          fontSize: 14,
-          marginLeft: 6,
-        },
-        foodItemDetails: {
-          flex: 1,
-        },
-        subDetails: {
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        },
-        favoriteIcon: {
-          paddingHorizontal: 5,
-        },
-        bottomRow: {
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        },
-        iconsContainer: {
-          flexDirection: 'row',
-          alignItems: 'center',
-        },
-        
-        quantityContainer: {
-          flexDirection: 'row',
-          alignItems: 'center',
-        },
-        itemQuantity: {
-          color: 'white',
-          paddingHorizontal: 5,
-        },
-        icons: {
-          paddingHorizontal: 5,
-        },
-        viewCartButton: {
-          position: 'absolute',
-          bottom: 66,
-          right: 16,
-          paddingHorizontal: 24,
-          paddingVertical: 8,
-          backgroundColor: 'dodgerblue',
-          borderRadius: 8,
-          paddingBottom: 8,
-        },
-        viewCartButtonText: {
-          fontSize: 16,
-          color: 'white',
-        },
-      });
-      
+            </View>
+          </View>
+        </View>
+      </LinearGradient>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  foodItemContainer: {
+    flex: 1,
+    marginBottom: 16,
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginTop: 5,
+    margin: 5,
+  },
+  foodItemImage: {
+    width: '100%',
+    height: 200,
+  },
+  foodItemOverlay: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    padding: 16,
+  },
+  foodItemDetails: {
+    flex: 1,
+  },
+  topRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  foodItemName: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  foodItemCategory: {
+    color: 'white',
+    fontSize: 14,
+    marginTop: 4,
+  },
+  favoriteIcon: {
+    paddingHorizontal: 5,
+  },
+  bottomRow: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+  iconsContainer: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  },
+  quantityContainer: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  },
+  itemQuantity: {
+  color: 'white',
+  paddingHorizontal: 5,
+  },
+  icons: {
+  paddingHorizontal: 5,
+  },
+  viewCartButton: {
+  position: 'absolute',
+  bottom: 66,
+  right: 16,
+  paddingHorizontal: 24,
+  paddingVertical: 8,
+  backgroundColor: 'dodgerblue',
+  borderRadius: 8,
+  paddingBottom: 8,
+  },
+  viewCartButtonText: {
+  fontSize: 16,
+  color: 'white',
+  },
+  });
