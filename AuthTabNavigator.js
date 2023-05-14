c8674421e71c202cb93d1b6e768706d2e17b12c5
Kushal Pagolu <kushal.pagolu@gmail.com>
2023-05-14 12:07:48 -0400

    added animation effect while switching tabs.

--- AuthTabNavigator.js
+++ AuthTabNavigator.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import {View,Text, TextInput, Image, TouchableOpacity, StyleSheet, Alert, KeyboardAvoidingView, Platform} from 'react-native';
+import React, { useRef } from 'react';
+import { Animated, Platform, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
-const AuthTabs = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 const AuthTabNavigator = ({ setIsLoggedIn }) => {
+  const loginAnimation = useRef(new Animated.Value(1)).current;
+  const signupAnimation = useRef(new Animated.Value(1)).current;
+
+  const animate = (animation) => {
+    Animated.timing(animation, {
+      toValue: 2,
+      duration: 100,
+      useNativeDriver: Platform.OS !== 'web',
+    }).start(() => {
+      Animated.timing(animation, {
+        toValue: 1,
+        duration: 200,
+        useNativeDriver: Platform.OS !== 'web', 
+      }).start();
+    });
+  };
+
   return (
-    <AuthTabs.Navigator
+    <Tab.Navigator
       screenOptions={({ route }) => ({
         headerShown: false,
         tabBarStyle: {
@@ -19,30 +36,65 @@ const AuthTabNavigator = ({ setIsLoggedIn }) => {
           right: 15,
           height: 60,
           borderRadius: 8,
-          paddingBottom: Platform.OS === 'ios' ? 15 : 0, // add this line
-
+          paddingBottom: Platform.OS === 'ios' ? 15 : 0,
         },
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
+        tabBarButton: ({ accessibilityState, onPress, onLongPress, children }) => {
+          let scale;
           let iconColor;
+          let iconName;
+          let labelColor;
+
           if (route.name === 'Login') {
-            iconName = focused ? 'sign-in-alt' : 'sign-in-alt';
-            iconColor = focused ? '#4db8ff' : '#888';
+            iconName = 'sign-in-alt';
+            iconColor = labelColor = accessibilityState.selected ? '#4db8ff' : '#888';
+            scale = loginAnimation;
           } else if (route.name === 'Signup') {
-            iconName = focused ? 'user-plus' : 'user-plus';
-            iconColor = focused ? '#4db8ff' : '#888';
+            iconName = 'user-plus';
+            iconColor = labelColor = accessibilityState.selected ? '#4db8ff' : '#888';
+            scale = signupAnimation;
           }
 
-          return <FontAwesome5 name={iconName} size={size} color={iconColor} />;
-        },
+          const transform = [{ scale }];
+
+          return (
+            <View
+              onStartShouldSetResponder={() => true}
+              onResponderRelease={onPress}
+              onLongPress={onLongPress}
+              style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+            >
+              <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                <Animated.View style={{ transform }}>
+                  <FontAwesome5 name={iconName} size={24} color={iconColor} />
+                </Animated.View>
+                <Animated.View style={{ transform, marginTop: 4 }}>
+                  <Text style={{ color: labelColor }}>{route.name}</Text>
+                </Animated.View>
+              </View>
+            </View>
+          );
+          
+        }
       })}
     >
-      <AuthTabs.Screen name="Login" options={{ headerShown: false }}>
+      <Tab.Screen 
+        name="Login"
+        listeners={{
+          tabPress: () => animate(loginAnimation),
+        }}
+      >
         {(props) => <LoginScreen {...props} setIsLoggedIn={setIsLoggedIn} />}
-      </AuthTabs.Screen>
-      <AuthTabs.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }} />
-    </AuthTabs.Navigator>
+      </Tab.Screen>
+      <Tab.Screen 
+        name="Signup" 
+        component={SignupScreen}
+        listeners={{
+          tabPress: () => animate(signupAnimation),
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
 export default AuthTabNavigator;
+
